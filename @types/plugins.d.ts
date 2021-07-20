00ebec3216d07c621b44e1cb718527c43962a088
Yoichi Kobayashi <info@tplh.net>
2021-07-21 02:24:05 +0900

    update the video plugin.

--- @types/plugins.d.ts
+++
@@ -1,7 +0,0 @@
-import Video from '@/utils/Video'
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $video: Video
-  }
-}
--- plugins/video.ts
+++ plugins/video.ts
@@ -1,5 +1,41 @@
 import { Plugin } from '@nuxt/types'
-import Video from '@/utils/Video'
+
+class Video {
+  elm: HTMLVideoElement | null
+
+  constructor() {
+    this.elm = null
+  }
+
+  async start(video: HTMLVideoElement) {
+    let srcObject = null
+    this.elm = video
+
+    await navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          width: 1920,
+          facingMode: 'user'
+        }
+      })
+      .then(stream => {
+        srcObject = stream
+      })
+      .catch(() => {})
+    
+    this.elm.srcObject = srcObject
+    this.elm.setAttribute('playsinline', 'playsinline')
+    // this.elm.setAttribute('controls', 'controls')
+    this.elm.play()
+  }
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $video: Video
+  }
+}
 
 const video: Plugin = ({}, inject) => {
   inject('video', new Video())

88dd73c44150fdf2cad6e58c289a4af9f95dcde5
Yoichi Kobayashi <info@tplh.net>
2021-07-22 22:57:39 +0900

    created Camera class.

---
+++ webgl/facemesh/Camera.ts
@@ -0,0 +1,14 @@
+import * as THREE from 'three'
+
+export default class Camera extends THREE.OrthographicCamera {
+  constructor(width: number, height: number) {
+    super(width / -2, width / 2, height / 2, height / -2, 0.1, 100)
+  }
+
+  resize(width: number, height: number) {
+    this.left = width / -2
+    this.right = width / 2
+    this.top = height / 2
+    this.bottom = height / -2
+  }
+}

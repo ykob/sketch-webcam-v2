829cd1a2926483b0e85a289b138e99781ece3544
yoichi kobayashi <koba@tsumikiinc.com>
2021-07-26 16:38:10 +0900

    created the video icon component.

---
+++ components/icon/Video.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+IconBase(
+  iconName = 'video'
+  v-bind = '$attrs'
+  )
+  path(
+    :d = 'd'
+    )
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mdiVideo } from '@mdi/js'
+
+export default Vue.extend({
+  computed: {
+    d(): string {
+      return mdiVideo
+    },
+  },
+})
+</script>

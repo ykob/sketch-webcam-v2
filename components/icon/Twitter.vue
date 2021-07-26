5e16ae044e74e379cc83547bfc86a2a73d84cfd3
yoichi kobayashi <koba@tsumikiinc.com>
2021-07-26 17:21:42 +0900

    created the twitter icon component.

---
+++ components/icon/Twitter.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+IconBase(
+  iconName = 'twitter'
+  v-bind = '$attrs'
+  )
+  path(
+    :d = 'd'
+    )
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mdiTwitter } from '@mdi/js'
+
+export default Vue.extend({
+  computed: {
+    d(): string {
+      return mdiTwitter
+    },
+  },
+})
+</script>

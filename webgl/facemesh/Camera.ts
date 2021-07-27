3fe6fc57e2dbd4a97b247866e2459b8211582745
Yoichi Kobayashi <info@tplh.net>
2021-07-28 01:41:36 +0900

    added the process to run updateProjectionMatrix in Camera.

--- webgl/facemesh/Camera.ts
+++ webgl/facemesh/Camera.ts
@@ -3,6 +3,8 @@ import * as THREE from 'three'
 export default class Camera extends THREE.OrthographicCamera {
   constructor(width: number, height: number) {
     super(width / -2, width / 2, height / 2, height / -2, 0.1, 100)
+    this.position.z = 1
+    this.lookAt(0, 0, 0)
   }
 
   resize(width: number, height: number) {
@@ -10,5 +12,6 @@ export default class Camera extends THREE.OrthographicCamera {
     this.right = width / 2
     this.top = height / 2
     this.bottom = height / -2
+    this.updateProjectionMatrix()
   }
 }

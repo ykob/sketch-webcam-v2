6a0a2753a4aa270b69576d6e4b90757f229ead17
Yoichi Kobayashi <info@tplh.net>
2021-08-05 17:57:46 +0900

    added the setupback component.

---
+++ components/_template/SetupBack.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.setup-back
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.setup-back {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: z(setupBack);
+  background-color: #222;
+}
+</style>

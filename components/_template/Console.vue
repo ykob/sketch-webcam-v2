8951d9be943ce5fc1644d531a9b3b9d8c8b9925f
Yoichi Kobayashi <info@tplh.net>
2021-08-05 16:51:15 +0900

    added the back index button.

---
+++ components/_template/Console.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+.console
+  ExampleOutline.console__outline(
+    :title = 'title'
+    )
+  ButtonBackIndex.console__back-index
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.console {
+  &__outline {
+    position: absolute;
+    z-index: z(console);
+    @include l-lg {
+      top: 40px;
+      left: 40px;
+    }
+    @include l-md {
+      top: 32px;
+      left: 32px;
+    }
+    @include l-sm {
+      top: 24px;
+      left: 24px;
+    }
+  }
+  &__back-index {
+    position: absolute;
+    z-index: z(console);
+    @include l-lg {
+      top: 40px;
+      right: 40px;
+    }
+    @include l-md {
+      top: 32px;
+      right: 32px;
+    }
+    @include l-sm {
+      top: 24px;
+      right: 24px;
+    }
+  }
+}
+</style>
---
+++ components/button/BackIndex.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+ButtonBase(
+  :width = 'size.width'
+  :height = 'size.width'
+  :radius = 'size.radius'
+  role = 'white'
+  tag = 'a'
+  href = '/'
+  @click = '$emit("click", $event)'
+  )
+  IconDotsVertical(
+    :size = 'iconSize'
+    )
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  computed: {
+    breakPoint(): string {
+      const { getters } = this.$store
+
+      return getters.breakpoint
+    },
+    size(): { [key: string]: string } {
+      const size = (): number => {
+        switch (this.breakPoint) {
+          case 'sm':
+            return 36
+          case 'md':
+            return 48
+          case 'lg':
+          default:
+            return 60
+        }
+      }
+      return {
+        width: `${size()}px`,
+        radius: `${size() / 2}px`,
+      }
+    },
+    iconSize(): number {
+      console.log(this.breakPoint)
+      switch (this.breakPoint) {
+        case 'sm':
+          return 24
+        case 'md':
+          return 32
+        case 'lg':
+        default:
+          return 40
+      }
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>

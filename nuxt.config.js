95b88f2e2f54f8ed00a8278a2d78abdb66bcf1c7
Yoichi Kobayashi <info@tplh.net>
2021-08-05 17:57:22 +0900

    added web font.

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;1,700&display=swap' },
     ]
   },
 

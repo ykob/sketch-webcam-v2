61a6dd85720d86271311c4784f9f06da19f44e11
Yoichi Kobayashi <info@tplh.net>
2021-09-04 22:00:37 +0900

    update the camera position.

--- webgl/glasses/Camera.ts
+++ webgl/glasses/Camera.ts
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 export default class Camera extends THREE.OrthographicCamera {
   constructor(width: number, height: number) {
     super(width / -2, width / 2, height / 2, height / -2, 0.1, 1000)
-    this.position.z = 1
+    this.position.z = 500
     this.lookAt(0, 0, 0)
   }
 
